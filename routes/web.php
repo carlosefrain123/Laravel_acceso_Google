fbb20b72e5063363f1029d069b137797c5c2736c
Efrain <carlosefrainchallo1@gmail.com>
2024-06-06 19:53:17 -0500

    06/06/2024 - 07:51 - (1) En la interfaz welcome se coloca solo elregistro y acceso a google - (2) Se mejora la codificación de google-auth/callback en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+// web.php
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -9,25 +9,28 @@ use Laravel\Socialite\Facades\Socialite;
 Route::get('/', function () {
     return view('welcome');
 });
-//Acceso a Google
+
+// Acceso a Google
 Route::get('/google-auth/redirect', function () {
     return Socialite::driver('google')->redirect();
 });
 
 Route::get('/google-auth/callback', function () {
     $user_google = Socialite::driver('google')->stateless()->user();
-    $user=User::updateOrCreate([
-        'google_id'=>$user_google->id,
-    ],[
-        'name'=>$user_google->name,
-        'email'=>$user_google->email,
-    ]
-    );
-    Auth::login($user);
-    return redirect('dashboard');
-    // $user->token
+    $user = User::updateOrCreate([
+        'email' => $user_google->email,
+    ], [
+        'name' => $user_google->name,
+        'google_id' => $user_google->id,
+        'email_verified_at' => now(),
+        'password' => null,  // Opcional, ya que no es necesario para Google Auth
+    ]);
+
+    Auth::login($user, true);  // 'true' para recordar al usuario
+    return redirect('/dashboard');
 });
-//
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -39,3 +42,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
