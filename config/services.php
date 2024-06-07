bd3b588e893865e84cf5e8cedb7af59a225d4036
Efrain <carlosefrainchallo1@gmail.com>
2024-06-06 19:10:10 -0500

    06/09/2024 - 7:00 - (1)Se agrega en services.php tanto el client_secret, el redirect y client_id de Google - (2)Se modifica la migración en la function up se agrega el google_id -(3) En la ruta web.php se coloca la codificación de login y redirección de google, para que pueda entrar a la página correspondiente que es el dashboard - (4) En el .env se agrega los ID de Google - (5) En welcome se agrega el boton de register de  google

--- config/services.php
+++ config/services.php
@@ -36,8 +36,8 @@ return [
     ],
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
-        'client_secret' => env('GITHUB_CLIENT_SECRET'),
-        'redirect' => 'http://example.com/callback-url',
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => 'http://limpiobe.com:8000/google-auth/callback',
     ],
 
 ];
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,33 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
 });
+//Acceso a Google
+Route::get('/google-auth/redirect', function () {
+    return Socialite::driver('google')->redirect();
+});
 
+Route::get('/google-auth/callback', function () {
+    $user_google = Socialite::driver('google')->stateless()->user();
+    $user=User::updateOrCreate([
+        'google_id'=>$user_google->id,
+    ],[
+        'name'=>$user_google->name,
+        'email'=>$user_google->email,
+    ]
+    );
+    Auth::login($user);
+    return redirect('dashboard');
+    // $user->token
+});
+//
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
